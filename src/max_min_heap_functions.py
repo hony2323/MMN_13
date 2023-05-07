70b36827a52d917f777c6a477c7a588feaac4222
hony <hony2323@gmail.com>
2023-05-07 20:51:45 +0300

    Changed to be modular functions

---
+++ src/max_min_heap_functions.py
@@ -0,0 +1,35 @@
+def heapify(a, i):
+    pass
+
+
+def build_heap(a):
+    pass
+
+
+def heap_extract_max(a):
+    pass
+
+
+def heap_extract_min(a):
+    pass
+
+
+def heap_insert(a, key):
+    pass
+
+
+def heap_delete(a, i):
+    pass
+
+
+# Private functions
+def _parent(a, i):
+    pass
+
+
+def _left(a, i):
+    pass
+
+
+def _right(a, i):
+    pass

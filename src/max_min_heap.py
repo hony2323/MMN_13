ded6538f49064ba1a8c2057ba85afec7bac87c92
hony <hony2323@gmail.com>
2023-05-07 20:37:26 +0300

    Added all the heap functions

---
+++ src/max_min_heap.py
@@ -0,0 +1,31 @@
+class MaxMinHeap:
+    def __init__(self):
+        pass
+
+    def heapify(self, i):
+        pass
+
+    def build_heap(self):
+        pass
+
+    def heap_extract_max(self):
+        pass
+
+    def heap_extract_min(self):
+        pass
+
+    def heap_insert(self, key):
+        pass
+
+    def heap_delete(self, i):
+        pass
+
+    # Private functions
+    def _parent(self, i):
+        pass
+
+    def _left(self, i):
+        pass
+
+    def _right(self, i):
+        pass

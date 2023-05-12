34f29cf7e26c795fc3db400214436dcc30219e9d
hony <hony2323@gmail.com>
2023-05-13 00:40:47 +0300

    one more checkpoint print works

--- src/menu.py
+++ src/menu.py
@@ -7,47 +7,51 @@ from max_min_heap import MaxMinHeap
 class Menu:
     def __init__(self, root_dir):
         root_dir = root_dir or os.path.dirname(os.path.abspath(__file__))
-        self.first_running = False
-        self.second_running = False
-
         self.mmh = MaxMinHeap([])
+        self.running = False
+        self.menu = self._first_menu()
 
     def start(self):
-        pass
+        self.running = True
+        while self.running:
+            self.print_menu()
+            self.handle_selection()
 
     # def handle_input(self, ):
     #     pass
 
-    def handle_selection(self, menu):
-        pass
+    def handle_selection(self):
+        a = input()
 
     # Menu initializations
     def _first_menu(self):
         return {
-            "1": (self._replace_heap_file, "write file path (relative to src/)"),
-            "2": (self._replace_heap_string, "write you heap in this format [x,x,x,x,x,x,x,....] (including [])"),
+            "1": (self._file_replace_heap, "write file path (relative to src/)"),
+            "2": (self._string_replace_heap, "write you heap in this format [x,x,x,x,x,x,x,....] (including [])"),
         }
 
     def _second_menu(self):
-        pass
+        return {}
 
-    # Helpers
-    def _replace_heap_file(self, path_to_file):
+    # ------ PART OF FIRST MENU ---------
+    def _file_replace_heap(self, path_to_file):
         try:
             with open(path_to_file, 'r') as f:
                 self.mmh = MaxMinHeap(json.load(f))
         except FileNotFoundError as e:
             print(f"\n file {path_to_file} not found \n")
             raise e
+        self.menu = self._second_menu()
 
-    def _replace_heap_string(self, s):
+    def _string_replace_heap(self, s):
         ls = json.loads(s)
         if not type(ls) == list:
             print("\nthe input is not a list\n")
             raise Exception()
         self.mmh = MaxMinHeap(json.loads(s))
+        self.menu = self._second_menu()
 
-    def print_menu(self, menu: dict):
-        for key in menu.keys():
-            func , msg = menu[key]
+    def print_menu(self):
+        for key in self.menu.keys():
+            func, msg = self.menu[key]
             print(f"{key} - {msg}")
